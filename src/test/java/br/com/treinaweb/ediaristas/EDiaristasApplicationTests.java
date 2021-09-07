2c3c6fd63b94c662c77316d924a385054161ac69
Cleyson Lima <cleysonph@gmail.com>
2021-09-07 05:57:38 -0300

    02:02 - criando o projeto

---
+++ src/test/java/br/com/treinaweb/ediaristas/EDiaristasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ediaristas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDiaristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
