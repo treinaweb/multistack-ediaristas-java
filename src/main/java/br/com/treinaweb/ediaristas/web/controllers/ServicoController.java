6aaba7a8f5aecf58448f7682b46dc12a19d048e7
Cleyson Lima <cleysonph@gmail.com>
2021-09-07 09:12:46 -0300

    02:07 - criando a view de cadastro de serviço

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -2,9 +2,11 @@ package br.com.treinaweb.ediaristas.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.treinaweb.ediaristas.core.enums.Icone;
 import br.com.treinaweb.ediaristas.core.models.Servico;
 
 @Controller
@@ -18,6 +20,11 @@ public class ServicoController {
         modelAndView.addObject("servico", new Servico());
 
         return modelAndView;
+    }
+
+    @ModelAttribute("icones")
+    public Icone[] getIcones() {
+        return Icone.values();
     }
     
 }
