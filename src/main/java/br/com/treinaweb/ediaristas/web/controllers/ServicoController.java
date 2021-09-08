6353647946b89fb37204d817dbe8ffeeb6bcebf1
Cleyson Lima <cleysonph@gmail.com>
2021-09-07 21:51:30 -0300

    02:10 - criando action de exclusão de serviço

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,6 +41,13 @@ public class ServicoController {
     @PostMapping("/cadastrar")
     public String cadastrar(Servico servico) {
         repository.save(servico);
+
+        return "redirect:/admin/servicos";
+    }
+
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+        repository.deleteById(id);
 
         return "redirect:/admin/servicos";
     }
