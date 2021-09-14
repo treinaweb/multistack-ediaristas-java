a026ee771cda50cd5e4f3f9334bdb1ca68e80436
Cleyson Lima <cleysonph@gmail.com>
2021-09-14 02:05:58 -0300

    02:09 - criando o dto de edição de usuário

--- src/main/java/br/com/treinaweb/ediaristas/web/mappers/WebUsuarioMapper.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/mappers/WebUsuarioMapper.java
@@ -5,6 +5,7 @@ import org.mapstruct.factory.Mappers;
 
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioEdicaoForm;
 
 @Mapper(componentModel = "spring")
 public interface WebUsuarioMapper {
@@ -12,5 +13,9 @@ public interface WebUsuarioMapper {
     WebUsuarioMapper INSTANCE = Mappers.getMapper(WebUsuarioMapper.class);
 
     Usuario toModel(UsuarioCadastroForm form);
+
+    Usuario toModel(UsuarioEdicaoForm form);
+
+    UsuarioEdicaoForm toForm(Usuario model);
     
 }
