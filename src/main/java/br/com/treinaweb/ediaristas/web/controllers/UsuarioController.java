f7a997eb962bc7dccd4385fadbe61a8c6a4f63e3
Cleyson Lima <cleysonph@gmail.com>
2021-09-14 01:34:54 -0300

    02:07 - criando a funcionalidade de cadastro de usuário

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
@@ -1,11 +1,18 @@
 package br.com.treinaweb.ediaristas.web.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.treinaweb.ediaristas.web.dtos.FlashMessage;
 import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
 import br.com.treinaweb.ediaristas.web.services.WebUsuarioService;
 
@@ -32,6 +39,22 @@ public class UsuarioController {
         modelAndView.addObject("cadastroForm", new UsuarioCadastroForm());
 
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(
+        @Valid @ModelAttribute("cadastroForm") UsuarioCadastroForm cadastroForm, 
+        BindingResult result, 
+        RedirectAttributes attrs
+    ) {
+        if (result.hasErrors()) {
+            return "admin/usuario/cadastro-form";
+        }
+
+        service.cadastrar(cadastroForm);
+        attrs.addFlashAttribute("alert", new FlashMessage("alert-succes", "Usuário cadastrado com sucesso!"));
+
+        return "redirect:/admin/usuarios";
     }
     
 }
--- src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.mappers.WebUsuarioMapper;
 
 @Service
 public class WebUsuarioService {
@@ -14,8 +17,19 @@ public class WebUsuarioService {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private WebUsuarioMapper mapper;
+
     public List<Usuario> buscarTodos() {
         return repository.findAll();
+    }
+
+    public Usuario cadastrar(UsuarioCadastroForm form) {
+        var model = mapper.toModel(form);
+
+        model.setTipoUsuario(TipoUsuario.ADMIN);
+
+        return repository.save(model);
     }
     
 }
