8efb9a506769fd65325cee7a01495388e11fdc6f
Cleyson Lima <cleysonph@gmail.com>
2021-09-15 23:10:18 -0300

    02:20 - atualizando a configuração do spring security

--- src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
@@ -1,17 +1,50 @@
 package br.com.treinaweb.ediaristas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+            .antMatchers("/admin/**").hasAuthority(TipoUsuario.ADMIN.toString())
+            .anyRequest().authenticated();
+
+        http.formLogin();
+
+        http.logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"));
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers("/webjar/**");
     }
     
 }
