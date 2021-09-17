41eceae12a486af8502ee8f8ccbfa22c5b3a764c
Cleyson Lima <cleysonph@gmail.com>
2021-09-17 01:23:03 -0300

    02:26 - ajustes finais

--- src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-            .antMatchers("/webjars/**");
+            .antMatchers("/webjars/**")
+            .antMatchers("/img/**");
     }
     
 }
