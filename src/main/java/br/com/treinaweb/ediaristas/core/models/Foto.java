b9d418e5e8656e514548f80829523c7a63b1179b
Cleyson Lima <cleysonph@gmail.com>
2021-12-06 20:23:14 -0300

    01:04 - criando o model de Foto

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/models/Foto.java
@@ -0,0 +1,41 @@
+package br.com.treinaweb.ediaristas.core.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class Foto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String filename;
+
+    @Column(name = "content_length", nullable = false)
+    private Long contentLength;
+
+    @Column(name = "content_type", nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private String url;
+
+}
