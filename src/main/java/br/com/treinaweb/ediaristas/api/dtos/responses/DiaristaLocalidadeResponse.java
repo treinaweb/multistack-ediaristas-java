8ebaf23cfb1b84b331a98bb4d1ec9644318d512c
Cleyson Lima <cleysonph@gmail.com>
2021-12-14 20:13:53 -0300

    corrigindo exibição da rota de listagem de diaristas por localidade

--- src/main/java/br/com/treinaweb/ediaristas/api/dtos/responses/DiaristaLocalidadeResponse.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/dtos/responses/DiaristaLocalidadeResponse.java
@@ -1,5 +1,8 @@
 package br.com.treinaweb.ediaristas.api.dtos.responses;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(SnakeCaseStrategy.class)
 public class DiaristaLocalidadeResponse {
 
     private String nomeCompleto;
