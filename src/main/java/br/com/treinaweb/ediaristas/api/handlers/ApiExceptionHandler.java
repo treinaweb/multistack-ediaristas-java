4a1dcdc69b4927eb4f4d9048e5d3e35bce6d89bb
Cleyson Lima <cleysonph@gmail.com>
2021-12-16 20:01:18 -0300

    01:06 - criando o método para tratar os erros de validação na API

--- src/main/java/br/com/treinaweb/ediaristas/api/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/handlers/ApiExceptionHandler.java
@@ -1,13 +1,22 @@
 package br.com.treinaweb.ediaristas.api.handlers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.treinaweb.ediaristas.api.dtos.responses.ErrorResponse;
@@ -15,6 +24,8 @@ import br.com.treinaweb.ediaristas.core.services.consultaendereco.exceptions.End
 
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private SnakeCaseStrategy camelCaseToSnakeCase = new SnakeCaseStrategy();
 
     @ExceptionHandler(EnderecoServiceException.class)
     public ResponseEntity<Object> handleEnderecoServiceException(
@@ -28,6 +39,32 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             .build();
 
         return ResponseEntity.badRequest().body(errorReponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException exception,
+        HttpHeaders headers,
+        HttpStatus status,
+        WebRequest request
+    ) {
+        var body = new HashMap<String, List<String>>();
+
+        exception.getBindingResult().getFieldErrors()
+            .forEach(fieldError -> {
+                var field = camelCaseToSnakeCase.translate(fieldError.getField());
+
+                if (!body.containsKey(field)) {
+                    var fieldErrors = new ArrayList<String>();
+                    fieldErrors.add(fieldError.getDefaultMessage());
+
+                    body.put(field, fieldErrors);
+                } else {
+                    body.get(field).add(fieldError.getDefaultMessage());
+                }
+            });
+
+        return ResponseEntity.badRequest().body(body);
     }
 
 }
