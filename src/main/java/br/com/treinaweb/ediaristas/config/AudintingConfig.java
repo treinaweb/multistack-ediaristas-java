61e3f742718787da4e953cd80679cb78eeb7bf95
Cleyson Lima <cleysonph@gmail.com>
2022-01-02 20:47:27 -0300

    01:04 - criando o modelo e repository da diária

---
+++ src/main/java/br/com/treinaweb/ediaristas/config/AudintingConfig.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.ediaristas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AudintingConfig {
+
+}
---
+++ src/main/java/br/com/treinaweb/ediaristas/core/models/Auditable.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.ediaristas.core.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
