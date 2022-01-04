aa5022a989ed3fa13c8891cfa18b4d41180d8e28
Cleyson Lima <cleysonph@gmail.com>
2022-01-04 20:31:13 -0300

    01:03 - criando o dto, controller e service de pagamento

---
+++ src/main/java/br/com/treinaweb/ediaristas/api/controllers/DiariaPagamentoRestController.java
@@ -0,0 +1,28 @@
+package br.com.treinaweb.ediaristas.api.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.ediaristas.api.dtos.requests.PagamentoRequest;
+import br.com.treinaweb.ediaristas.api.dtos.responses.MensagemResponse;
+import br.com.treinaweb.ediaristas.api.services.ApiDiariaPagamentoService;
+
+@RestController
+@RequestMapping("/api/diarias/{id}")
+public class DiariaPagamentoRestController {
+
+    @Autowired
+    private ApiDiariaPagamentoService service;
+
+    @PostMapping("/pagar")
+    public MensagemResponse pagar(@RequestBody @Valid PagamentoRequest request, @PathVariable Long id) {
+        return service.pagar(request, id);
+    }
+
+}
---
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiDiariaPagamentoService.java
@@ -0,0 +1,34 @@
+package br.com.treinaweb.ediaristas.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.treinaweb.ediaristas.api.dtos.requests.PagamentoRequest;
+import br.com.treinaweb.ediaristas.api.dtos.responses.MensagemResponse;
+import br.com.treinaweb.ediaristas.core.enums.DiariaStatus;
+import br.com.treinaweb.ediaristas.core.exceptions.DiariaNaoEncontradaException;
+import br.com.treinaweb.ediaristas.core.models.Diaria;
+import br.com.treinaweb.ediaristas.core.repositories.DiariaRepository;
+
+@Service
+public class ApiDiariaPagamentoService {
+
+    @Autowired
+    private DiariaRepository diariaRepository;
+
+    public MensagemResponse pagar(PagamentoRequest request, Long id) {
+        var diaria = buscarDiariaPorId(id);
+
+        diaria.setStatus(DiariaStatus.PAGO);
+        diariaRepository.save(diaria);
+
+        return new MensagemResponse("Diária paga com sucesso!");
+    }
+
+    private Diaria buscarDiariaPorId(Long id) {
+        var mensagem = String.format("Diária com id %d não encontrada", id);
+        return diariaRepository.findById(id)
+            .orElseThrow(() -> new DiariaNaoEncontradaException(mensagem));
+    }
+
+}
