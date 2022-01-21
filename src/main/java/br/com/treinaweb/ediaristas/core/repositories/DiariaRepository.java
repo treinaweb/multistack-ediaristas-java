882ad55e9f9e2660995a8076f174b5b0fedc6a92
Cleyson Lima <cleysonph@gmail.com>
2022-01-20 21:05:54 -0300

    01:10 - criando a query de busca de oportunidades

--- src/main/java/br/com/treinaweb/ediaristas/core/repositories/DiariaRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/repositories/DiariaRepository.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.ediaristas.core.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.treinaweb.ediaristas.core.models.Diaria;
 import br.com.treinaweb.ediaristas.core.models.Usuario;
@@ -12,5 +13,25 @@ public interface DiariaRepository extends JpaRepository<Diaria, Long> {
     List<Diaria> findByCliente(Usuario cliente);
 
     List<Diaria> findByDiarista(Usuario diarista);
+
+    @Query(
+        """
+        SELECT
+            d
+        FROM
+            Diaria d
+        WHERE
+            d.status = br.com.treinaweb.ediaristas.core.enums.DiariaStatus.PAGO
+        AND
+            d.diarista IS NULL
+        AND
+            d.codigoIbge IN :cidades
+        AND
+            :candidato NOT MEMBER OF d.candidatos
+        AND
+            SIZE(d.candidatos) < 3
+        """
+    )
+    List<Diaria> findOportunidades(List<String> cidades, Usuario candidato);
 
 }
