68803f17e1d9bf62d4beb169c767b04cbe44c7bb
Cleyson Lima <cleysonph@gmail.com>
2022-01-25 16:39:30 -0300

    01:11 - validando a confirmação de presença

--- src/main/java/br/com/treinaweb/ediaristas/api/services/ApiConfirmacaoPresencaService.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiConfirmacaoPresencaService.java
@@ -8,6 +8,7 @@ import br.com.treinaweb.ediaristas.core.enums.DiariaStatus;
 import br.com.treinaweb.ediaristas.core.exceptions.DiariaNaoEncontradaException;
 import br.com.treinaweb.ediaristas.core.models.Diaria;
 import br.com.treinaweb.ediaristas.core.repositories.DiariaRepository;
+import br.com.treinaweb.ediaristas.core.validators.ConfirmacaoPresencaValidator;
 
 @Service
 public class ApiConfirmacaoPresencaService {
@@ -15,8 +16,12 @@ public class ApiConfirmacaoPresencaService {
     @Autowired
     private DiariaRepository diariaRepository;
 
+    @Autowired
+    private ConfirmacaoPresencaValidator validator;
+
     public MensagemResponse confirmarPresenca(Long id) {
         var diaria = buscarDiariaPorId(id);
+        validator.validar(diaria);
         diaria.setStatus(DiariaStatus.CONCLUIDO);
         diariaRepository.save(diaria);
         return new MensagemResponse("Presença confirmada com sucesso!");
