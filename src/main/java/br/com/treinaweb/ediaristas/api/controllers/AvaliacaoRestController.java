3d8215ae2546520f2caa2aa8c68065c2af5b35d6
Cleyson Lima <cleysonph@gmail.com>
2022-01-25 22:52:06 -0300

    01:06 - criando a funcionalidade de criar uma avaliação

---
+++ src/main/java/br/com/treinaweb/ediaristas/api/controllers/AvaliacaoRestController.java
@@ -0,0 +1,32 @@
+package br.com.treinaweb.ediaristas.api.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.ediaristas.api.dtos.requests.AvaliacaoRequest;
+import br.com.treinaweb.ediaristas.api.dtos.responses.MensagemResponse;
+import br.com.treinaweb.ediaristas.api.services.ApiAvaliacaoService;
+import br.com.treinaweb.ediaristas.core.permissions.EDiaristasPermissions;
+
+@RestController
+@RequestMapping("/api/diarias/{id}/avaliacao")
+public class AvaliacaoRestController {
+
+    @Autowired
+    private ApiAvaliacaoService service;
+
+    @PatchMapping
+    @EDiaristasPermissions.isClienteOrDiaristaDaDiaria
+    public MensagemResponse avaliarDiaria(
+        @RequestBody @Valid AvaliacaoRequest request, @PathVariable Long id
+    ) {
+        return service.avaliarDiaria(request, id);
+    }
+
+}
---
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiAvaliacaoService.java
@@ -0,0 +1,59 @@
+package br.com.treinaweb.ediaristas.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.treinaweb.ediaristas.api.dtos.requests.AvaliacaoRequest;
+import br.com.treinaweb.ediaristas.api.dtos.responses.MensagemResponse;
+import br.com.treinaweb.ediaristas.api.mappers.ApiAvaliacaoMapper;
+import br.com.treinaweb.ediaristas.core.exceptions.DiariaNaoEncontradaException;
+import br.com.treinaweb.ediaristas.core.models.Avaliacao;
+import br.com.treinaweb.ediaristas.core.models.Diaria;
+import br.com.treinaweb.ediaristas.core.models.Usuario;
+import br.com.treinaweb.ediaristas.core.repositories.AvaliacaoRepository;
+import br.com.treinaweb.ediaristas.core.repositories.DiariaRepository;
+import br.com.treinaweb.ediaristas.core.utils.SecurityUtils;
+
+@Service
+public class ApiAvaliacaoService {
+
+    @Autowired
+    private AvaliacaoRepository repository;
+
+    @Autowired
+    private SecurityUtils securityUtils;
+
+    @Autowired
+    private DiariaRepository diariaRepository;
+
+    @Autowired
+    private ApiAvaliacaoMapper mapper;
+
+    public MensagemResponse avaliarDiaria(AvaliacaoRequest request, Long id) {
+        var diaria = buscarDiariaPorId(id);
+        var avaliador = securityUtils.getUsuarioLogado();
+
+        var model = mapper.toModel(request);
+        model.setAvaliador(avaliador);
+        model.setDiaria(diaria);
+        model.setVisibilidade(true);
+        model.setAvaliado(getAvaliado(model));
+        repository.save(model);
+
+        return new MensagemResponse("Avaliação realizada com sucesso!");
+    }
+
+    private Usuario getAvaliado(Avaliacao model) {
+        if (model.getAvaliador().isCliente()) {
+            return model.getDiaria().getDiarista();
+        }
+        return model.getDiaria().getCliente();
+    }
+
+    private Diaria buscarDiariaPorId(Long id) {
+        var mensagem = String.format("Diária com id %d não encontrada", id);
+        return diariaRepository.findById(id)
+            .orElseThrow(() -> new DiariaNaoEncontradaException(mensagem));
+    }
+
+}
