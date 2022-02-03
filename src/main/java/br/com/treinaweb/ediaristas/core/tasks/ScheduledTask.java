344f75104e7c3d7eec2ab70cf99f89fc415952b7
Cleyson Lima <cleysonph@gmail.com>
2022-02-02 22:41:49 -0300

    01:05 - criando a task de cancelamento de diárias

--- src/main/java/br/com/treinaweb/ediaristas/core/tasks/ScheduledTask.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/tasks/ScheduledTask.java
@@ -9,6 +9,7 @@ import br.com.treinaweb.ediaristas.core.enums.DiariaStatus;
 import br.com.treinaweb.ediaristas.core.models.Diaria;
 import br.com.treinaweb.ediaristas.core.repositories.DiariaRepository;
 import br.com.treinaweb.ediaristas.core.services.diaristaindice.adapters.DiaristaIndiceService;
+import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.adpaters.GatewayPagamentoService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,6 +22,9 @@ public class ScheduledTask {
     @Autowired
     private DiaristaIndiceService diaristaIndiceService;
 
+    @Autowired
+    private GatewayPagamentoService gatewayPagamentoService;
+
     @Scheduled(cron = "0 0/5 * * * ?")
     @Transactional(readOnly = false)
     public void selecionarDiaristaDaDiaria() {
@@ -32,6 +36,17 @@ public class ScheduledTask {
         log.info("Task de seleção de diarista finalizada");
     }
 
+    @Scheduled(cron = "0 3/5 * * * ?")
+    @Transactional(readOnly = false)
+    public void cancelarDiariasSemCandidatos() {
+        log.info("Iniciada task de cancelamento de diárias sem candidatos");
+
+        var diariasAptasParaCancelamento = diariaRepository.getAptasParaCancelamento();
+        diariasAptasParaCancelamento.stream().forEach(this::cancelarDiaria);
+
+        log.info("Finalizada task de cancelamento de diárias sem candidatos");
+    }
+
     private void selecionarDiarista(Diaria diaria) {
         log.info("Selecionando melhor diárista para diária de id " + diaria.getId().toString());
         var melhorDiarista = diaristaIndiceService.selecionarMelhorDiarista(diaria);
@@ -39,6 +54,17 @@ public class ScheduledTask {
         diaria.setStatus(DiariaStatus.CONFIRMADO);
         diariaRepository.save(diaria);
         log.info("Selecionado o diárista de id " + melhorDiarista.getId().toString());
+    }
+
+    private void cancelarDiaria(Diaria diaria) {
+        log.info("Cancelando diária de id " + diaria.getId());
+        if (diaria.isPago()) {
+            log.info("Reembolsando pagamento da diária de id " + diaria.getId());
+            gatewayPagamentoService.realizarEstornoTotal(diaria);
+        }
+        diaria.setStatus(DiariaStatus.CANCELADO);
+        diariaRepository.save(diaria);
+        log.info("Diária de id " + diaria.getId() + " cancelada com sucesso");
     }
 
 }
