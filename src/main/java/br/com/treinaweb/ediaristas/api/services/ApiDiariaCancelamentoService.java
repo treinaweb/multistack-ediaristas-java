468fe685400bf7a9ad4124bed1922662f86e616e
Cleyson Lima <cleysonph@gmail.com>
2022-02-10 15:04:57 -0300

    01:05 - verificando se deve haver penalização no cancelamento da diária

--- src/main/java/br/com/treinaweb/ediaristas/api/services/ApiDiariaCancelamentoService.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiDiariaCancelamentoService.java
@@ -1,5 +1,8 @@
 package br.com.treinaweb.ediaristas.api.services;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,7 @@ import br.com.treinaweb.ediaristas.core.enums.DiariaStatus;
 import br.com.treinaweb.ediaristas.core.exceptions.DiariaNaoEncontradaException;
 import br.com.treinaweb.ediaristas.core.models.Diaria;
 import br.com.treinaweb.ediaristas.core.repositories.DiariaRepository;
+import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.adpaters.GatewayPagamentoService;
 import br.com.treinaweb.ediaristas.core.validators.DiariaCancelamentoValidator;
 
 @Service
@@ -20,6 +24,9 @@ public class ApiDiariaCancelamentoService {
     @Autowired
     private DiariaCancelamentoValidator validator;
 
+    @Autowired
+    private GatewayPagamentoService gatewayPagamentoService;
+
     public MensagemResponse cancelar(Long diariaId, DiariaCancelamentoRequest request) {
         var diaria = buscarDiariaPorId(diariaId);
         validator.validar(diaria);
@@ -28,13 +35,27 @@ public class ApiDiariaCancelamentoService {
         diaria.setMotivoCancelamento(request.getMotivoCancelamento());
         diariaRepository.save(diaria);
 
+        if (hasPenalizacao(diaria)) {
+            aplicarPenalizacao(diaria);
+        } else {
+            gatewayPagamentoService.realizarEstornoTotal(diaria);
+        }
+
         return new MensagemResponse("A diária foi cancelado com sucesso!");
+    }
+
+    private void aplicarPenalizacao(Diaria diaria) {
     }
 
     private Diaria buscarDiariaPorId(Long diariaId) {
         var mensagem = String.format("Diária com id %d não encontrada", diariaId);
         return diariaRepository.findById(diariaId)
             .orElseThrow(() -> new DiariaNaoEncontradaException(mensagem));
+    }
+
+    private boolean hasPenalizacao(Diaria diaria) {
+        var hoje = LocalDateTime.now();
+        return ChronoUnit.HOURS.between(hoje, diaria.getDataAtendimento()) < 24;
     }
 
 }
