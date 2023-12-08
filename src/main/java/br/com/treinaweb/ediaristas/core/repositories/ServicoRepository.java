768c9e54baf9918aa467eb9f7f8b401c7c58ca77
Cleyson <cleysonph@gmail.com>
2023-12-08 20:49:03 -0300

    clean project

--- src/main/java/br/com/treinaweb/ediaristas/core/repositories/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.treinaweb.ediaristas.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.treinaweb.ediaristas.core.models.Servico;
-
-public interface ServicoRepository extends JpaRepository<Servico, Long> {
-    
-}
